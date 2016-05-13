c6ffd69c30ce5061a8c61baddfaddd6ba853382f
Brendon <bdtower8@gmail.com>
2016-05-13 10:08:25 -0400

    minize code for readability

--- SplashScreenGenerator/main.cpp
+++ SplashScreenGenerator/main.cpp
@@ -1,29 +1,8 @@
 #include <QCoreApplication>
 #include <QFile>
 #include <QImage>
+#include <QMap>
 #include <QDebug>
-
-class SizeNamePair {
-public:
-    SizeNamePair() {}
-
-    SizeNamePair(const QString &sFilename, const QSize& size) {
-        m_sFilename = sFilename;
-        m_size = size;
-    }
-
-    SizeNamePair(const SizeNamePair& rhs) {
-        m_sFilename = rhs.Filename();
-        m_size = rhs.Size();
-    }
-
-    QString Filename() const { return m_sFilename; }
-    QSize Size() const { return m_size; }
-
-private:
-    QString m_sFilename;
-    QSize m_size;
-};
 
 int main(int argc, char *argv[])
 {
@@ -35,9 +14,9 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     QString sImageFilename = a.arguments().at(1);
 
-    QVector<SizeNamePair> vImageSizes;
-    vImageSizes.push_back(SizeNamePair("small.png", QSize(480, 640)));
-    vImageSizes.push_back(SizeNamePair("large.png", QSize(1080, 1920)));
+    QMap<QString, QSize> mapImageSizes;
+    mapImageSizes.insert("small.png", QSize(480, 640));
+    mapImageSizes.insert("large.png", QSize(1080, 1920));
 
     if(QFile::exists(sImageFilename)) {
         // open the input image
@@ -45,23 +24,23 @@ int main(int argc, char *argv[])
         QImage imageOrig(sImageFilename);
 
         // for all of the desired sizes
-        foreach(const SizeNamePair thisSize, vImageSizes) {
+        foreach(const QString& key, mapImageSizes.keys()) {
             // scale the image down (make it larger to preserve aspect ratio)
-            QImage thisImage = imageOrig.scaled(thisSize.Size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+            QImage thisImage = imageOrig.scaled(mapImageSizes[key], Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
 
             // if we had to grow the image to preserve the aspect, cut off the edges
-            int iExtraWidth = thisImage.width() - thisSize.Size().width();
-            int iExtraHeight = thisImage.height() - thisSize.Size().height();
+            int iExtraWidth = thisImage.width() - mapImageSizes[key].width();
+            int iExtraHeight = thisImage.height() - mapImageSizes[key].height();
 
             // center the image
-            QRect rectCrop(iExtraWidth / 2, iExtraHeight / 2, thisSize.Size().width(), thisSize.Size().height());
+            QRect rectCrop(iExtraWidth / 2, iExtraHeight / 2, mapImageSizes[key].width(), mapImageSizes[key].height());
             QImage thisImageCropped = thisImage.copy(rectCrop);
 
             // save the image
-            thisImageCropped.save(thisSize.Filename());
+            thisImageCropped.save(key);
 
-            qDebug() << "Saved " + thisSize.Filename() + " (" + QString::number(thisSize.Size().width()) +
-                        " x " + QString::number(thisSize.Size().height()) + ")";
+            qDebug() << "Saved " + key + " (" + QString::number(mapImageSizes[key].width()) +
+                        " x " + QString::number(mapImageSizes[key].height()) + ")";
         }
 
     } else {
@@ -71,4 +50,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
